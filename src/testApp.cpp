264baa9aa47de56129711fea581e9019a5a01576
Alex Hornbake <alex@hornbake.com>
2012-04-29 11:29:33 -0400

    First Import of already existing files
    Starting from the OpenNi example, and added ARToolkit functionality.
    Currently have ARToolkit recognizing markers from the Kinect RGB
    camera.

---
+++ src/testApp.cpp
@@ -0,0 +1,564 @@
+#include "testApp.h"
+
+
+
+//--------------------------------------------------------------
+void testApp::setup() {
+
+    width = 640;
+	height = 480;
+    
+	isLive			= true;
+	isTracking		= false;
+	isTrackingHands	= true;
+	isFiltering		= false;
+	isRecording		= false;
+	isCloud			= false;
+	isCPBkgnd		= true;
+	isMasking		= true;
+
+	nearThreshold = 500;
+	farThreshold  = 1000;
+
+	filterFactor = 0.1f;
+
+	setupRecording();
+
+    colorImage.allocate(width, height);
+	grayImage.allocate(width, height);
+	grayThres.allocate(width, height);
+	
+	// Load the image we are going to distort
+	displayImage.loadImage("of.jpg");
+	// Load the corners of the image into the vector
+	int displayImageHalfWidth = displayImage.width / 2;
+	int displayImageHalfHeight = displayImage.height / 2;	
+	displayImageCorners.push_back(ofPoint(0, 0));
+	displayImageCorners.push_back(ofPoint(displayImage.width, 0));
+	displayImageCorners.push_back(ofPoint(displayImage.width, displayImage.height));
+	displayImageCorners.push_back(ofPoint(0, displayImage.height));	
+	
+	// This uses the default camera calibration and marker file
+	artk.setup(width, height);
+    
+	// The camera calibration file can be created using GML:
+	// http://graphics.cs.msu.ru/en/science/research/calibration/cpp
+	// and these instructions:
+	// http://studierstube.icg.tu-graz.ac.at/doc/pdf/Stb_CamCal.pdf
+	// This only needs to be done once and will aid with detection
+	// for the specific camera you are using
+	// Put that file in the data folder and then call setup like so:
+	// artk.setup(width, height, "myCamParamFile.cal", "markerboard_480-499.cfg");
+	
+	// Set the threshold
+	// ARTK+ does the thresholding for us
+	// We also do it in OpenCV so we can see what it looks like for debugging
+	threshold = 85;
+	artk.setThreshold(threshold);
+    
+	ofBackground(0, 0, 0);
+
+}
+
+void testApp::setupRecording(string _filename) {
+
+#if defined (TARGET_OSX) //|| defined(TARGET_LINUX) // only working on Mac/Linux at the moment (but on Linux you need to run as sudo...)
+	hardware.setup();				// libusb direct control of motor, LED and accelerometers
+	hardware.setLedOption(LED_OFF); // turn off the led just for yacks (or for live installation/performances ;-)
+#endif
+
+	recordContext.setup();	// all nodes created by code -> NOT using the xml config file at all
+	//recordContext.setupUsingXMLFile();
+	recordDepth.setup(&recordContext);
+	recordImage.setup(&recordContext);
+
+	recordUser.setup(&recordContext);
+	recordUser.setSmoothing(filterFactor);				// built in openni skeleton smoothing...
+	recordUser.setUseMaskPixels(isMasking);
+	recordUser.setUseCloudPoints(isCloud);
+	recordUser.setMaxNumberOfUsers(2);					// use this to set dynamic max number of users (NB: that a hard upper limit is defined by MAX_NUMBER_USERS in ofxUserGenerator)
+
+	recordHandTracker.setup(&recordContext, 4);
+	recordHandTracker.setSmoothing(filterFactor);		// built in openni hand track smoothing...
+	recordHandTracker.setFilterFactors(filterFactor);	// custom smoothing/filtering (can also set per hand with setFilterFactor)...set them all to 0.1f to begin with
+
+	recordContext.toggleRegisterViewport();
+	recordContext.toggleMirror();
+
+	oniRecorder.setup(&recordContext, ONI_STREAMING);
+	//oniRecorder.setup(&recordContext, ONI_CYCLIC, 60);
+	//read the warning in ofxOpenNIRecorder about memory usage with ONI_CYCLIC recording!!!
+
+}
+
+void testApp::setupPlayback(string _filename) {
+
+	playContext.shutdown();
+	playContext.setupUsingRecording(ofToDataPath(_filename));
+	playDepth.setup(&playContext);
+	playImage.setup(&playContext);
+
+	playUser.setup(&playContext);
+	playUser.setSmoothing(filterFactor);				// built in openni skeleton smoothing...
+	playUser.setUseMaskPixels(isMasking);
+	playUser.setUseCloudPoints(isCloud);
+
+	playHandTracker.setup(&playContext, 4);
+	playHandTracker.setSmoothing(filterFactor);			// built in openni hand track smoothing...
+	playHandTracker.setFilterFactors(filterFactor);		// custom smoothing/filtering (can also set per hand with setFilterFactor)...set them all to 0.1f to begin with
+
+	playContext.toggleRegisterViewport();
+	playContext.toggleMirror();
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+#ifdef TARGET_OSX // only working on Mac at the moment
+	hardware.update();
+#endif
+
+	if (isLive) {
+
+		// update all nodes
+		recordContext.update();
+		recordDepth.update();
+		recordImage.update();
+
+		// demo getting depth pixels directly from depth gen
+		depthRangeMask.setFromPixels(recordDepth.getDepthPixels(nearThreshold, farThreshold),
+									 recordDepth.getWidth(), recordDepth.getHeight(), OF_IMAGE_GRAYSCALE);
+
+		// update tracking/recording nodes
+		if (isTracking) recordUser.update();
+		if (isRecording) oniRecorder.update();
+
+		// demo getting pixels from user gen
+		if (isTracking && isMasking) {
+			allUserMasks.setFromPixels(recordUser.getUserPixels(), recordUser.getWidth(), recordUser.getHeight(), OF_IMAGE_GRAYSCALE);
+			user1Mask.setFromPixels(recordUser.getUserPixels(1), recordUser.getWidth(), recordUser.getHeight(), OF_IMAGE_GRAYSCALE);
+			user2Mask.setFromPixels(recordUser.getUserPixels(2), recordUser.getWidth(), recordUser.getHeight(), OF_IMAGE_GRAYSCALE);
+		}
+        
+        
+        
+        //colorImage.setFromPixels(recordImage.getPixels(), width, height);
+		colorImage.setFromPixels(recordImage.getPixels(), recordUser.getWidth(), recordUser.getHeight());
+        colorImage.mirror(false,true);
+        
+		// convert our camera image to grayscale
+		grayImage = colorImage;
+		// apply a threshold so we can see what is going on
+		grayThres = grayImage;
+		grayThres.threshold(threshold);
+		
+		// Pass in the new image pixels to artk
+		artk.update(grayImage.getPixels());
+
+	} else {
+
+		// update all nodes
+		playContext.update();
+		playDepth.update();
+		playImage.update();
+
+		// demo getting depth pixels directly from depth gen
+		depthRangeMask.setFromPixels(playDepth.getDepthPixels(nearThreshold, farThreshold),
+									 playDepth.getWidth(), playDepth.getHeight(), OF_IMAGE_GRAYSCALE);
+
+		// update tracking/recording nodes
+		if (isTracking) playUser.update();
+
+		// demo getting pixels from user gen
+		if (isTracking && isMasking) {
+			allUserMasks.setFromPixels(playUser.getUserPixels(), playUser.getWidth(), playUser.getHeight(), OF_IMAGE_GRAYSCALE);
+			user1Mask.setFromPixels(playUser.getUserPixels(1), playUser.getWidth(), playUser.getHeight(), OF_IMAGE_GRAYSCALE);
+			user2Mask.setFromPixels(playUser.getUserPixels(2), playUser.getWidth(), playUser.getHeight(), OF_IMAGE_GRAYSCALE);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	ofSetColor(255, 255, 255);
+
+	//glPushMatrix();
+	//glScalef(0.75, 0.75, 0.75);
+
+	if (isLive) {
+        
+		recordDepth.draw(0,0,640,480);
+		recordImage.draw(640, 0, 640, 480);
+
+		depthRangeMask.draw(0, 480, 320, 240);	// can use this with openCV to make masks, find contours etc when not dealing with openNI 'User' like objects
+
+		if (isTracking) {
+			recordUser.draw();
+
+			if (isMasking) drawMasks();
+			if (isCloud) drawPointCloud(&recordUser, 1);	// 0 gives you all point clouds; use userID to see point clouds for specific users
+
+		}
+		if (isTrackingHands)
+			recordHandTracker.drawHands();
+        
+        // Main image
+        ofSetHexColor(0xffffff);
+        //grayImage.draw(0, 0);
+        ofSetHexColor(0x666666);	
+        ofDrawBitmapString(ofToString(artk.getNumDetectedMarkers()) + " marker(s) found", 10, 20);
+        
+        // Threshold image
+        ofSetHexColor(0xffffff);
+        //grayThres.draw(640, 0);
+        ofSetHexColor(0x666666);	
+        ofDrawBitmapString("Threshold: " + ofToString(threshold), 650, 20);
+        ofDrawBitmapString("Use the Up/Down keys to adjust the threshold", 650, 40);
+        
+        // ARTK draw
+        // An easy was to see what is going on
+        // Draws the marker location and id number
+        artk.draw(640, 0);
+        
+        // ARTK 2D stuff
+        // See if marker ID '0' was detected
+        // and draw a circle in the center with the ID number.
+        int myIndex = artk.getMarkerIndex(0);
+        ofPoint center;
+        if(myIndex >= 0) {	
+            // Can also get the center like this:
+            center = artk.getDetectedMarkerCenter(myIndex);
+            ofSetHexColor(0x0000ff);
+            ofCircle(width-center.x+640,center.y,10);
+            ofSetHexColor(0xffffff);
+            ofDrawBitmapString("0",width-center.x+640,center.y,10);
+        }
+        // See if marker ID '1' was detected
+        // and draw a circle in the center with the ID number.
+        myIndex = artk.getMarkerIndex(1);
+        if(myIndex >= 0) {	
+            // Can also get the center like this:
+            center = artk.getDetectedMarkerCenter(myIndex);
+            ofSetHexColor(0x0000ff);
+            ofCircle(width-center.x+640,center.y,10);
+            ofSetHexColor(0xffffff);
+            ofDrawBitmapString("1",width-center.x+640,center.y,10);
+        }
+
+	} else {
+
+		playDepth.draw(0,0,640,480);
+		playImage.draw(640, 0, 640, 480);
+
+		depthRangeMask.draw(0, 480, 320, 240);	// can use this with openCV to make masks, find contours etc when not dealing with openNI 'User' like objects
+
+		if (isTracking) {
+			playUser.draw();
+
+			if (isMasking) drawMasks();
+			if (isCloud) drawPointCloud(&playUser, 0);	// 0 gives you all point clouds; use userID to see point clouds for specific users
+
+		}
+		if (isTrackingHands)
+			playHandTracker.drawHands();
+	}
+
+	//glPopMatrix();
+
+	ofSetColor(255, 255, 0);
+
+	string statusPlay		= (string)(isLive ? "LIVE STREAM" : "PLAY STREAM");
+	string statusRec		= (string)(!isRecording ? "READY" : "RECORDING");
+	string statusSkeleton	= (string)(isTracking ? "TRACKING USERS: " + (string)(isLive ? ofToString(recordUser.getNumberOfTrackedUsers()) : ofToString(playUser.getNumberOfTrackedUsers())) + "" : "NOT TRACKING USERS");
+	string statusSmoothSkel = (string)(isLive ? ofToString(recordUser.getSmoothing()) : ofToString(playUser.getSmoothing()));
+	string statusHands		= (string)(isTrackingHands ? "TRACKING HANDS: " + (string)(isLive ? ofToString(recordHandTracker.getNumTrackedHands()) : ofToString(playHandTracker.getNumTrackedHands())) + ""  : "NOT TRACKING");
+	string statusFilter		= (string)(isFiltering ? "FILTERING" : "NOT FILTERING");
+	string statusFilterLvl	= ofToString(filterFactor);
+	string statusSmoothHand = (string)(isLive ? ofToString(recordHandTracker.getSmoothing()) : ofToString(playHandTracker.getSmoothing()));
+	string statusMask		= (string)(!isMasking ? "HIDE" : (isTracking ? "SHOW" : "YOU NEED TO TURN ON TRACKING!!"));
+	string statusCloud		= (string)(isCloud ? "ON" : "OFF");
+	string statusCloudData	= (string)(isCPBkgnd ? "SHOW BACKGROUND" : (isTracking ? "SHOW USER" : "YOU NEED TO TURN ON TRACKING!!"));
+
+	string statusHardware;
+
+#ifdef TARGET_OSX // only working on Mac at the moment
+	ofPoint statusAccelerometers = hardware.getAccelerometers();
+	stringstream	statusHardwareStream;
+
+	statusHardwareStream
+	<< "ACCELEROMETERS:"
+	<< " TILT: " << hardware.getTiltAngle() << "/" << hardware.tilt_angle
+	<< " x - " << statusAccelerometers.x
+	<< " y - " << statusAccelerometers.y
+	<< " z - " << statusAccelerometers.z;
+
+	statusHardware = statusHardwareStream.str();
+#endif
+
+	stringstream msg;
+
+	msg
+	<< "    s : start/stop recording  : " << statusRec << endl
+	<< "    p : playback/live streams : " << statusPlay << endl
+	<< "    t : skeleton tracking     : " << statusSkeleton << endl
+	<< "( / ) : smooth skely (openni) : " << statusSmoothSkel << endl
+	<< "    h : hand tracking         : " << statusHands << endl
+	<< "    f : filter hands (custom) : " << statusFilter << endl
+	<< "[ / ] : filter hands factor   : " << statusFilterLvl << endl
+	<< "; / ' : smooth hands (openni) : " << statusSmoothHand << endl
+	<< "    m : drawing masks         : " << statusMask << endl
+	<< "    c : draw cloud points     : " << statusCloud << endl
+	<< "    b : cloud user data       : " << statusCloudData << endl
+	<< "- / + : nearThreshold         : " << ofToString(nearThreshold) << endl
+	<< "< / > : farThreshold          : " << ofToString(farThreshold) << endl
+	<< endl
+	<< "File  : " << oniRecorder.getCurrentFileName() << endl
+	<< "FPS   : " << ofToString(ofGetFrameRate()) << "  " << statusHardware << endl;
+
+	ofDrawBitmapString(msg.str(), 20, 560);
+
+}
+
+void testApp:: drawMasks() {
+	glPushMatrix();
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
+	allUserMasks.draw(640, 0, 640, 480);
+	glDisable(GL_BLEND);
+    glPopMatrix();
+	user1Mask.draw(320, 480, 320, 240);
+	user2Mask.draw(640, 480, 320, 240);
+	
+}
+
+void testApp::drawPointCloud(ofxUserGenerator * user_generator, int userID) {
+
+	glPushMatrix();
+
+	int w = user_generator->getWidth();
+	int h = user_generator->getHeight();
+
+	glTranslatef(w, h/2, -500);
+	ofRotateY(pointCloudRotationY);
+
+	glBegin(GL_POINTS);
+
+	int step = 1;
+
+	for(int y = 0; y < h; y += step) {
+		for(int x = 0; x < w; x += step) {
+			ofPoint pos = user_generator->getWorldCoordinateAt(x, y, userID);
+			if (pos.z == 0 && isCPBkgnd) continue;	// gets rid of background -> still a bit weird if userID > 0...
+			ofColor color = user_generator->getWorldColorAt(x,y, userID);
+			glColor4ub((unsigned char)color.r, (unsigned char)color.g, (unsigned char)color.b, (unsigned char)color.a);
+			glVertex3f(pos.x, pos.y, pos.z);
+		}
+	}
+
+	glEnd();
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glPopMatrix();
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+	float smooth;
+
+	switch (key) {
+#ifdef TARGET_OSX // only working on Mac at the moment
+		case 357: // up key
+			//hardware.setTiltAngle(hardware.tilt_angle++);
+            artk.setThreshold(++threshold);
+			break;
+		case 359: // down key
+			//hardware.setTiltAngle(hardware.tilt_angle--);
+            artk.setThreshold(--threshold);
+			break;
+			
+#endif
+		case 's':
+		case 'S':
+			if (isRecording) {
+				oniRecorder.stopRecord();
+				isRecording = false;
+				break;
+			} else {
+				oniRecorder.startRecord(generateFileName());
+				isRecording = true;
+				break;
+			}
+			break;
+		case 'p':
+		case 'P':
+			if (oniRecorder.getCurrentFileName() != "" && !isRecording && isLive) {
+				setupPlayback(oniRecorder.getCurrentFileName());
+				isLive = false;
+			} else {
+				isLive = true;
+			}
+			break;
+		case 't':
+		case 'T':
+			isTracking = !isTracking;
+			break;
+		case 'h':
+		case 'H':
+			isTrackingHands = !isTrackingHands;
+			if(isLive) recordHandTracker.toggleTrackHands();
+			if(!isLive) playHandTracker.toggleTrackHands();
+			break;
+		case 'f':
+		case 'F':
+			isFiltering = !isFiltering;
+			recordHandTracker.isFiltering = isFiltering;
+			playHandTracker.isFiltering = isFiltering;
+			break;
+		case 'm':
+		case 'M':
+			isMasking = !isMasking;
+			recordUser.setUseMaskPixels(isMasking);
+			playUser.setUseMaskPixels(isMasking);
+			break;
+		case 'c':
+		case 'C':
+			isCloud = !isCloud;
+			recordUser.setUseCloudPoints(isCloud);
+			playUser.setUseCloudPoints(isCloud);
+			break;
+		case 'b':
+		case 'B':
+			isCPBkgnd = !isCPBkgnd;
+			break;
+		case '9':
+		case '(':
+			smooth = recordUser.getSmoothing();
+			if (smooth - 0.1f > 0.0f) {
+				recordUser.setSmoothing(smooth - 0.1f);
+				playUser.setSmoothing(smooth - 0.1f);
+			}
+			break;
+		case '0':
+		case ')':
+			smooth = recordUser.getSmoothing();
+			if (smooth + 0.1f <= 1.0f) {
+				recordUser.setSmoothing(smooth + 0.1f);
+				playUser.setSmoothing(smooth + 0.1f);
+			}
+			break;
+		case '[':
+		//case '{':
+			if (filterFactor - 0.1f > 0.0f) {
+				filterFactor = filterFactor - 0.1f;
+				recordHandTracker.setFilterFactors(filterFactor);
+				if (oniRecorder.getCurrentFileName() != "") playHandTracker.setFilterFactors(filterFactor);
+			}
+			break;
+		case ']':
+		//case '}':
+			if (filterFactor + 0.1f <= 1.0f) {
+				filterFactor = filterFactor + 0.1f;
+				recordHandTracker.setFilterFactors(filterFactor);
+				if (oniRecorder.getCurrentFileName() != "") playHandTracker.setFilterFactors(filterFactor);
+			}
+			break;
+		case ';':
+		case ':':
+			smooth = recordHandTracker.getSmoothing();
+			if (smooth - 0.1f > 0.0f) {
+				recordHandTracker.setSmoothing(smooth -  0.1f);
+				playHandTracker.setSmoothing(smooth -  0.1f);
+			}
+			break;
+		case '\'':
+		case '\"':
+			smooth = recordHandTracker.getSmoothing();
+			if (smooth + 0.1f <= 1.0f) {
+				recordHandTracker.setSmoothing(smooth +  0.1f);
+				playHandTracker.setSmoothing(smooth +  0.1f);
+			}
+			break;
+		case '>':
+		case '.':
+			farThreshold += 50;
+			if (farThreshold > recordDepth.getMaxDepth()) farThreshold = recordDepth.getMaxDepth();
+			break;
+		case '<':
+		case ',':
+			farThreshold -= 50;
+			if (farThreshold < 0) farThreshold = 0;
+			break;
+
+		case '+':
+		case '=':
+			nearThreshold += 50;
+			if (nearThreshold > recordDepth.getMaxDepth()) nearThreshold = recordDepth.getMaxDepth();
+			break;
+
+		case '-':
+		case '_':
+			nearThreshold -= 50;
+			if (nearThreshold < 0) nearThreshold = 0;
+			break;
+		case 'r':
+			recordContext.toggleRegisterViewport();
+			break;
+		default:
+			break;
+	}
+}
+
+string testApp::generateFileName() {
+
+	string _root = "kinectRecord";
+
+	string _timestamp = ofToString(ofGetDay()) +
+	ofToString(ofGetMonth()) +
+	ofToString(ofGetYear()) +
+	ofToString(ofGetHours()) +
+	ofToString(ofGetMinutes()) +
+	ofToString(ofGetSeconds());
+
+	string _filename = (_root + _timestamp + ".oni");
+
+	return _filename;
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+	if (isCloud) pointCloudRotationY = x;
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
